b1bf8fe542f578498571f643a084438352c4f148
Johannes Maier <johannes.maier@mailbox.org>
2023-10-29 23:06:57 +0100

    Parse the message source

--- src/kenranbot.cr
+++ src/kenranbot.cr
@@ -1,13 +1,54 @@
 require "http/web_socket"
 
+record MessageSource, nickname : String | Nil, host : String
+
 def handle_ping(msg)
   puts "PING received:"
   puts msg
 end
 
 def handle_message(msg)
-  puts "message received:"
-  puts msg
+  puts "message received"
+  msgs = msg.split("\r\n", remove_empty: true)
+  msgs.map { |msg| puts parse_message msg }
+end
+
+def parse_message_source(msg)
+  if msg[0] == ':'
+    next_space = msg.index(" ", 1)
+    if next_space
+      raw_message_source = msg[1...next_space]
+      parts = raw_message_source.split("!")
+      if parts.size == 2
+        source = MessageSource.new(parts[0], parts[1])
+      else
+        source = MessageSource.new(nil, parts[0])
+      end
+      {source: source, remaining: msg[next_space + 1..]}
+    else
+      {source: nil, remaining: msg}
+    end
+  else
+    {source: nil, remaining: msg}
+  end
+end
+
+def parse_message(msg)
+  if msg[0] == '@'
+    puts "cannot handle tags yet, dying"
+    return
+  end
+
+  source_res = parse_message_source msg
+  puts "parsed source: " + source_res.to_s
+  rem = source_res[:remaining]
+
+  command_end = rem.index(":")
+  if command_end
+    command = rem[0...command_end]
+    puts "command: " + command
+    puts "parameters: " + rem[command_end + 1..]
+  end
 end
 
 class Bot
